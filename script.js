4ab235c94f43ffdfdff617cbb2de30d8b375f2c5
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2022-11-14 09:39:40 +0100

    endcard

--- script.js
+++ script.js
@@ -102,22 +102,14 @@ let questionsCSS = [
 
 let pageCounter;
 let question;
+let currentDeck;
+let points;
 
 //Loading question-deck by selection in menu
 function init(questionDeck) {
     document.getElementById('card-body').innerHTML = questionCoverHTML(questionDeck);
     pageCounter = 0;
-}
-
-//return frontcover HTML of question-deck
-function questionCoverHTML(questionDeck) {
-    return `
-        <div class="card-body text-center background-img top-padding">
-          <h2 class="card-title">Welcome To <br> The Awesome ${questionDeck} Quiz</h2>
-          <p class="card-text font-size-text">Ready for the challenge?</p>
-          <button class="btn btn-primary button-position" onclick="questionValidator('${questionDeck}')" type="submit">START NOW <img src="img/White arrow right.png"></button>
-        </div>
-    `;
+    points = 0;
 }
 
 //determine which Array of Questions should be displayed
@@ -134,57 +126,24 @@ function questionValidator(id) {
   if (id == 'JAVA') {
     question = questionsJAVA; 
   }
+  currentDeck = id;
   showCurrentQuestion();
 }
 
-//generating HTML of individual questions
-function showCurrentQuestion() {
-    document.getElementById('card-body').innerHTML = `
-    <div class="card-body text-center background-question-container">
-            <h4 class="card-title">${question[pageCounter].question}</h4>
-            <div id="answer_1" class="card card-style mb-2" onclick="answer('answer_1')">
-              <div class="card-flex ">
-                <div id="letter-answer_1" class="letter-div"><b>A</b></div>
-                ${question[pageCounter].answer_1}
-              </div>
-            </div>
-            <div id="answer_2" class="card card-style mb-2" onclick="answer('answer_2')">
-              <div class="card-flex ">
-                <div id="letter-answer_2" class="letter-div"><b>B</b></div>
-                ${question[pageCounter].answer_2}
-              </div>
-            </div>
-            <div id="answer_3" class="card card-style mb-2" onclick="answer('answer_3')">
-              <div class="card-flex ">
-                <div id="letter-answer_3" class="letter-div"><b>C</b></div>
-                ${question[pageCounter].answer_3}
-              </div>
-            </div>
-            <div id="answer_4" class="card card-style mb-2" onclick="answer('answer_4')">
-              <div class="card-flex ">
-                <div id="letter-answer_4" class="letter-div"><b>D</b></div>
-                ${question[pageCounter].answer_4}
-              </div>
-            </div>
-            <div class="flex-display">
-              <span>Frage ${pageCounter + 1} von ${question.length} </span>
-              <button id="next-button" class="btn btn-primary" onclick="nextPage()" type="submit" disabled>Nächste Frage</button>
-            </div>
-        </div>
-    `;
-}
-
-//checking if right answer is selected
+//checking if right answer is selected and changing answer button colors
 function answer(param) {
+  buttonDisabled();
   if(param == question[pageCounter].right_answer){
     document.getElementById(param).classList.add('bg-green');
     document.getElementById('letter-' + param).classList.add('bg-green-letter');
+    points++;
   } else {
     document.getElementById(param).classList.add('bg-red');
     document.getElementById('letter-' + param).classList.add('bg-red-letter');
     document.getElementById(question[pageCounter].right_answer).classList.add('bg-green');
     document.getElementById('letter-' + question[pageCounter].right_answer).classList.add('bg-green-letter');
   }
+  //enabling Button to proceed to next question
   document.getElementById('next-button').disabled = false;
 }
 
@@ -193,5 +152,75 @@ function nextPage() {
   pageCounter++;
   if(pageCounter < question.length) {
     showCurrentQuestion()
+  } else endCardHTML();
+}
+
+//disabling answer buttons after an answer has been selected
+function buttonDisabled() {
+  for(let i = 1; i < 5; i++){
+  document.getElementById('answer_'+i).onclick = null;
   }
+}
+
+//----------------HTML--TEMPLATES-----------------------------------------------------------------
+
+//return frontcover HTML of question-deck
+function questionCoverHTML(questionDeck) {
+  return `
+      <div class="card-body text-center background-img top-padding">
+        <h2 class="card-title">Welcome To <br> The Awesome ${questionDeck} Quiz</h2>
+        <p class="card-text font-size-text">Ready for the challenge?</p>
+        <button class="btn btn-primary button-position" onclick="questionValidator('${questionDeck}')" type="submit">START NOW <img src="img/White arrow right.png"></button>
+      </div>
+  `;
+}
+
+//generating HTML of individual questions
+function showCurrentQuestion() {
+  document.getElementById('card-body').innerHTML = `
+  <div class="card-body text-center background-question-container">
+          <h4 class="card-title">${question[pageCounter].question}</h4>
+          <div id="answer_1" class="card card-style mb-2" onclick="answer('answer_1')">
+            <div class="card-flex ">
+              <div id="letter-answer_1" class="letter-div"><b>A</b></div>
+              ${question[pageCounter].answer_1}
+            </div>
+          </div>
+          <div id="answer_2" class="card card-style mb-2" onclick="answer('answer_2')">
+            <div class="card-flex ">
+              <div id="letter-answer_2" class="letter-div"><b>B</b></div>
+              ${question[pageCounter].answer_2}
+            </div>
+          </div>
+          <div id="answer_3" class="card card-style mb-2" onclick="answer('answer_3')">
+            <div class="card-flex ">
+              <div id="letter-answer_3" class="letter-div"><b>C</b></div>
+              ${question[pageCounter].answer_3}
+            </div>
+          </div>
+          <div id="answer_4" class="card card-style mb-2" onclick="answer('answer_4')">
+            <div class="card-flex ">
+              <div id="letter-answer_4" class="letter-div"><b>D</b></div>
+              ${question[pageCounter].answer_4}
+            </div>
+          </div>
+          <div class="flex-display">
+            <span>Frage ${pageCounter + 1} von ${question.length} </span>
+            <button id="next-button" class="btn btn-primary" onclick="nextPage()" type="submit" disabled>Nächste Frage</button>
+          </div>
+      </div>
+  `;
+}
+
+//generating result page HTML of question-deck
+function endCardHTML() {
+document.getElementById('card-body').innerHTML = `
+    <div class="card-body text-center background-question-container">
+      <img class="result-image" src="img/brain result.png">
+      <h5 class="card-title"><b>COMPLETE<br>${currentDeck} QUIZ</b></h5>
+      <h5 class="card-title color-orange">YOUR SCORE</h5>
+      <h5 class="card-title">${points}/${question.length}</h5>
+      <button class="btn btn-primary" onclick="init('${currentDeck}')" type="submit">REPLAY</button>
+    </div>
+`;
 }
