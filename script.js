952c475872df416bb5eef9df91dddbfd460eab40
Marco von Baumbach <marco.vonbaumbach@gmx.de>
2022-11-17 10:35:20 +0100

    responsive

--- script.js
+++ script.js
@@ -201,6 +201,10 @@ let points;
 let param1;
 let param2;
 let param3;
+let SUCCESS_AUDIO = new Audio('audio/success.mp3');
+let FAIL_AUDIO = new Audio('audio/fail.mp3');
+let FINISH_AUDIO = new Audio('audio/finish.mp3');
+
 
 //Loading question-deck by selection in menu
 function init(questionDeck) {
@@ -228,18 +232,20 @@ function questionValidator(id) {
   showCurrentQuestion();
 }
 
-//checking if right answer is selected and changing answer button colors
+//checking if right answer is selected and playing audio and changing answer button colors
 function answer(param) {
   buttonDisabled();
   if(param == question[pageCounter].right_answer){
     document.getElementById(param).classList.add('bg-green');
     document.getElementById('letter-' + param).classList.add('bg-green-letter');
+    SUCCESS_AUDIO.play();
     points++;
   } else {
     document.getElementById(param).classList.add('bg-red');
     document.getElementById('letter-' + param).classList.add('bg-red-letter');
     document.getElementById(question[pageCounter].right_answer).classList.add('bg-green');
     document.getElementById('letter-' + question[pageCounter].right_answer).classList.add('bg-green-letter');
+    FAIL_AUDIO.play();
   }
   //enabling Button to proceed to next question
   document.getElementById('next-button').disabled = false;
@@ -251,7 +257,10 @@ function nextPage() {
   progressBar();
   if(pageCounter < question.length) {
     showCurrentQuestion()
-  } else endCardHTML();
+  } else {
+    endCardHTML();
+    FINISH_AUDIO.play();
+  }
 }
 
 //disabling answer buttons after an answer has been selected
@@ -261,8 +270,39 @@ function buttonDisabled() {
   }
 }
 
+//Change menu font color to white wenn selected
+function selectMENU(par1, par2, par3) {
+
+  param1 = par1;
+  param2 = par2;
+  param3 = par3;
+  
+  document.getElementById(par1).classList.add('flex-menu-hover');
+  document.getElementById(par2).classList.add('flex-menu-hover');
+  document.getElementById(par3).classList.add('flex-menu-hover');
+}
+
+//Change menu font color to default when other menu is selected
+function leaveMENU() {
+  document.getElementById(param1).classList.remove('flex-menu-hover');
+  document.getElementById(param2).classList.remove('flex-menu-hover');
+  document.getElementById(param3).classList.remove('flex-menu-hover');
+}
+
+//resetting progress bar if on question deck cover, and moving it forward after each question
+function progressBar() {
+  if (pageCounter == 0){
+    document.getElementById('progress-bar').style = `width: 0%`;
+  } else {
+    let percent = pageCounter/question.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').style = `width: ${percent}%`;
+  }
+}
+
 
 //----------------HTML--TEMPLATES-----------------------------------------------------------------
+
 
 //return frontcover HTML of question-deck
 function questionCoverHTML(questionDeck) {
@@ -320,39 +360,8 @@ document.getElementById('card-body').innerHTML = `
       <h5 class="card-title"><b>COMPLETE<br>${currentDeck} QUIZ</b></h5>
       <h5 class="card-title color-orange">YOUR SCORE</h5>
       <h5 class="card-title">${points}/${question.length}</h5>
-      <button class="btn btn-primary" onclick="init('${currentDeck}')" type="submit">REPLAY</button>
+      <button class="btn btn-primary end-button" onclick="init('${currentDeck}')" type="submit">REPLAY</button>
     </div>     
     <img class="trophy" src="img/trophy.png">
 `;
 }
-
-//Change menu font color to white
-function selectMENU(par1, par2, par3) {
-
-  param1 = par1;
-  param2 = par2;
-  param3 = par3;
-  
-  document.getElementById(par1).classList.add('flex-menu-hover');
-  document.getElementById(par2).classList.add('flex-menu-hover');
-  document.getElementById(par3).classList.add('flex-menu-hover');
-}
-
-//Change menu font color to default
-function leaveMENU() {
-  document.getElementById(param1).classList.remove('flex-menu-hover');
-  document.getElementById(param2).classList.remove('flex-menu-hover');
-  document.getElementById(param3).classList.remove('flex-menu-hover');
-}
-
-//resetting progress bar if on question deck cover, and moving it forward after each question
-function progressBar() {
-  if (pageCounter == 0){
-    document.getElementById('progress-bar').style = `width: 0%`;
-  } else {
-    let percent = pageCounter/question.length;
-    percent = Math.round(percent * 100);
-    document.getElementById('progress-bar').style = `width: ${percent}%`;
-  }
-}
-
